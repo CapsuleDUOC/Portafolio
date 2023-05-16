70ba9037783450b79bd93f02543b44acc0bc4ced
clhidalgo <chidalgo@facele.cl>
2023-05-16 14:09:26 -0400

    annotations clase principal

--- webapps/feriavirtual/src/main/java/cl/duoc/portafolio/feriavirtual/FeriavirtualApplication.java
+++ webapps/feriavirtual/src/main/java/cl/duoc/portafolio/feriavirtual/FeriavirtualApplication.java
@@ -2,8 +2,12 @@ package cl.duoc.portafolio.feriavirtual;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = { "cl.duoc.portafolio.feriavirtual" })
+@EnableScheduling
+@EnableAsync
 public class FeriavirtualApplication {
 
 	public static void main(String[] args) {
