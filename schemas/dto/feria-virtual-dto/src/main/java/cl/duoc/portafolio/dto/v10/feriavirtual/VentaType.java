0af352b2032b39eb164b6062ddebb7267af07713
Claudio Hidalgo <cl.hidalgoc@duocuc.cl>
2023-05-21 20:40:51 -0400

    estadoArchivo en dto

--- schemas/dto/feria-virtual-dto/src/main/java/cl/duoc/portafolio/dto/v10/feriavirtual/VentaType.java
+++ schemas/dto/feria-virtual-dto/src/main/java/cl/duoc/portafolio/dto/v10/feriavirtual/VentaType.java
@@ -2,7 +2,7 @@
 // Este archivo ha sido generado por Eclipse Implementation of JAXB v2.3.7 
 // Visite https://eclipse-ee4j.github.io/jaxb-ri 
 // Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
-// Generado el: 2023.05.21 a las 08:19:40 PM CLT 
+// Generado el: 2023.05.21 a las 08:40:24 PM CLT 
 //
 
 
