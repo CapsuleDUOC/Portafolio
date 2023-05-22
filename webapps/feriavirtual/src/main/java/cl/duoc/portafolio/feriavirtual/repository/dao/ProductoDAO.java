8b1c0483db7a4f5de0b017c9d4668852eca0e1d4
Claudio Hidalgo <cl.hidalgoc@duocuc.cl>
2023-05-21 22:15:31 -0400

    annotation de repository en producto

--- webapps/feriavirtual/src/main/java/cl/duoc/portafolio/feriavirtual/repository/dao/ProductoDAO.java
+++ webapps/feriavirtual/src/main/java/cl/duoc/portafolio/feriavirtual/repository/dao/ProductoDAO.java
@@ -13,11 +13,13 @@ import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Repository;
 
 import cl.duoc.portafolio.feriavirtual.domain.Producto;
 import cl.duoc.portafolio.feriavirtual.repository.IProductoDAO;
 import cl.duoc.portafolio.feriavirtual.util.SearchCriteria;
 
+@Repository
 public class ProductoDAO implements IProductoDAO{
 
 	@PersistenceContext
