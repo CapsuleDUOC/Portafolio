fd053b66dedba513da99c54df104aad1495e6979
Claudio Hidalgo <cl.hidalgoc@duocuc.cl>
2023-05-21 21:02:08 -0400

    producto actualizar con bytes de imagen

--- schemas/dto/feria-virtual-dto/src/main/java/cl/duoc/portafolio/dto/v10/feriavirtual/package-info.java
+++ schemas/dto/feria-virtual-dto/src/main/java/cl/duoc/portafolio/dto/v10/feriavirtual/package-info.java
@@ -2,7 +2,7 @@
 // Este archivo ha sido generado por Eclipse Implementation of JAXB v2.3.7 
 // Visite https://eclipse-ee4j.github.io/jaxb-ri 
 // Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
-// Generado el: 2023.05.21 a las 08:40:24 PM CLT 
+// Generado el: 2023.05.21 a las 09:00:27 PM CLT 
 //
 
 @javax.xml.bind.annotation.XmlSchema(namespace = "http://duoc.cl/portafolio/dto/v10/FeriaVirtual")
